69898c3664221baad08dd92f5d4b88e244972f41
sunshine-22 <sabarishkumar2002@gmail.com>
2023-08-23 00:00:24 +0530

    initial commit

--- main.py
+++ main.py
@@ -5,35 +5,49 @@ from tortoise.contrib.fastapi import register_tortoise
 import casbin_tortoise_adapter
 from tortoise import Tortoise
 import casbin
-
+import uuid
 app = FastAPI()
 
+register_tortoise(
+    app,
+    db_url="postgres://root:root@localhost:5432/test_db",
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
-async def get_access(request : Request):
-    method = request.method
-    path = dict(request).get("path")
-    await Tortoise.init(
-        db_url="postgres://root:root@192.168.145.169:5432/test_db",
-        modules={"models": ["models","aerich.models"]},
-    )
-    await Tortoise.generate_schemas()
-    e = casbin.Enforcer('model.conf',adapter=casbin_tortoise_adapter.TortoiseAdapter())
-    sub = "member"  # the user that wants to access a resource.
-    obj = path  # the resource that is going to be accessed.
-    act = method  # the operation that the user performs on the resource.
-    print(sub,obj,act)
-    print(e.enforce(sub,obj,act))
 
 @app.get("/get_data")
-async def get_all_contacts(user = Depends(get_access)):
-    data = await models.Contact.all().values()
-    return data
+async def get_all_contacts():
+    
+    return {"hi":"response"}
 
+
+
+@app.post("/add_user")
+async def add_user(name:str):
+    add_user =  await models.usertable.create(name = name)
+    return {"response":"created"}
+
+@app.post("/country")
+async def add_user(name:str,country_name:str):
+    add_user =  await models.usertable.get(name = name)
+    country  =  await models.Country.create(country_name = country_name, created_by = add_user)
+    return {"response":"created"}
+
+@app.post("/state")
+async def add_user(name:str,country_name:str,state_name : str):
+    add_user =  await models.usertable.get(name = name)
+    print(">>>>>>>zcc>>>",add_user)
+    country =  await models.Country.get(country_name = country_name)
+    print("?????????????????",country)
+    state_add = await models.State.create(state_name = state_name,created_by = add_user,country = country)
+    return {"response":"created"}
 
 if __name__ == "__main__":
     uvicorn.run(
         host = "0.0.0.0",
         port=8000,
-        reload=True
+      
     )
--- migrations/models/0_20230822234123_init.py
+++ migrations/models/0_20230822234123_init.py
@@ -6,7 +6,16 @@ async def upgrade(db: BaseDBAsyncClient) -> str:
         CREATE TABLE IF NOT EXISTS "contact" (
     "id" SERIAL NOT NULL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
-    "phone" VARCHAR(20) NOT NULL
+    "phone_number" VARCHAR(20) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "usertable" (
+    "id" UUID NOT NULL  PRIMARY KEY,
+    "name" VARCHAR(100) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "country" (
+    "id" UUID NOT NULL  PRIMARY KEY,
+    "country_name" VARCHAR(100) NOT NULL,
+    "created_by_id" UUID REFERENCES "usertable" ("id") ON DELETE SET NULL
 );
 CREATE TABLE IF NOT EXISTS "aerich" (
     "id" SERIAL NOT NULL PRIMARY KEY,
